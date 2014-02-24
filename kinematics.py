c432f059ab86347d909e9ec20cc495c26a73757c
Emanuel Raad <raademang@gmail.com>
2014-02-23 19:22:09 -0500

    Made pretty and fixed float rounding error.

--- kinematics.py
+++ kinematics.py
@@ -13,7 +13,6 @@ toSort1 = ['vf', 'vi', 'a', 't', 'd']
 toSort2 = ['vf', 'vi', 'a', 't', 'd']
 checkList = ['a t vf vi', 'd t vf vi', 'a d t vi', 'a d t vf', 'a d vf vi']
 
-
 def eq1():
     return Eq(data[0], data[1]+data[2]*data[3])
 def eq2():
@@ -38,7 +37,7 @@ unknowns = 0
 print "Press enter if the value is unknown."
 for i in range(5):
     try:
-        data[i] = int(raw_input(prompt[i]))
+        data[i] = float(raw_input(prompt[i]))
     except:
         unknowns = unknowns + 1
 
@@ -46,7 +45,7 @@ if unknowns > 2:
     print "I can't solve this, there are too many unknown variables :("
 else:
     for i in data:
-        if isinstance(i, int):
+        if isinstance(i, float):
             pass
         else:
             known.remove(str(i))
@@ -64,24 +63,26 @@ else:
     for i in checkList:
         if i == a:
             steps1 = i
-            ans1 = '{0} is {1}'.format(solveFor[0], solve(options[checkList.index(i)](), solveFor[0]))
+            ans1 = '{0} is {1:.4f}'.format(solveFor[0], float(solve(options[checkList.index(i)](), solveFor[0])[0]))
 
     for i in checkList:
         if i == b:
             steps2 = i
-            ans2 = '{0} is {1}'.format(solveFor[1], solve(options[checkList.index(i)](), solveFor[1]))
+            ans2 = '{0} is {1:.4f}'.format(solveFor[1], float(solve(options[checkList.index(i)](), solveFor[1])[0]))
             
     data[0], data[1], data[2], data[3], data[4] = symbols('vf vi a t d')
     
     print
-    print "Rewrote to solve for {0}".format(solveFor[0])
-    print options[checkList.index(steps1)]()
-    print "Into:"
-    print solve(options[checkList.index(steps1)](), solveFor[0])
+    print "Rewrote:"
+    pprint(options[checkList.index(steps1)]())
+    print
+    print "To solve for {0}:".format(solveFor[0])
+    pprint(solve(options[checkList.index(steps1)](), solveFor[0]))
     print ans1
     print
-    print "Rewrote to solve for {0}".format(solveFor[1])
-    print options[checkList.index(steps2)]()
-    print "Into:"
-    print solve(options[checkList.index(steps2)](), solveFor[1])
+    print "Rewrote:"
+    pprint(options[checkList.index(steps2)]())
+    print
+    print "To solve for {0}:".format(solveFor[1])
+    pprint(solve(options[checkList.index(steps2)](), solveFor[1]))
     print ans2
