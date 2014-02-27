30ae65c001cea53aa0452a463fea2465a11714b0
Emanuel Raad <raademang@gmail.com>
2014-02-27 18:39:04 -0500

    Improved vector addition. Added function file, components.py

--- directionToDegree.py
+++ directionToDegree.py
@@ -1,11 +1,16 @@
-# Ex input: N 30 E
-# Ex output: 60
 import math
-a = raw_input('magnitude n direction pls, k thanks: ')
-b = a.split()
 
-b[0] = float(b[0])
-b[2] = float(b[2])
+b = raw_input('Enter a vector: ').split()
+xcomp, ycomp = 0,0
+
+try:
+	b[0] = float(b[0])
+	b[2] = float(b[2])
+except IndexError:
+	pass
+except ValueError:
+	b = 'b'
+	
 def directionToDegree(dir1, angle, dir2):
     directions = 'enws'
     plus = 'nw se en ws'
@@ -17,11 +22,17 @@ def directionToDegree(dir1, angle, dir2):
         newAngle = newAngle + angle
     else:
         newAngle = newAngle - angle
-    print newAngle
-    return newAngle
-    
-c = math.radians(directionToDegree(b[1], b[2], b[3]))
-xcomp = math.cos(c) * b[0]
-ycomp = math.sin(c) * b[0]
+    #return newAngle
+    newAngle = math.radians(newAngle)
+    return math.cos(newAngle) * b[0], math.sin(newAngle) * b[0]
+
+if len(b) == 2:
+	xcomp, ycomp = directionToDegree(b[1], 0, '')
+elif len(b) == 4:
+	xcomp, ycomp = directionToDegree(b[1], b[2], b[3])
+else:
+	print "You didn't enter a valid vector."
+	print "Expected: Magtinude, Direction 1, Angle, Direction 2"
+
 print 'xcomp: {0:.2f}'.format(xcomp)
 print 'ycomp: {0:.2f}'.format(ycomp)
